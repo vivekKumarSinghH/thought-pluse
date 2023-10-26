fe9e8001097203ec0d121e54c3f263968fa726e4
Vivek Kumar <72158561+vivekKumarSinghH@users.noreply.github.com>
2023-10-26 17:01:51 +0530

    feat:plus: add marketing page

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+import { Heading } from "./_components/heading";
+
+const MarketingPage = () => {
+  return (
+    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
+    <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+      <Heading />
+    </div>
+    
+  </div>
+  );
+};
+export default MarketingPage;
