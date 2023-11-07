53e3a0abb3b16221fcfee8d4e112b5af07282f96
Vivek Kumar <72158561+vivekKumarSinghH@users.noreply.github.com>
2023-11-07 22:33:40 +0530

    :green_heart: add document page

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="h-full flex dark:bg-[#1F1F1F]"></div>;
+};
+
+export default MainLayout;
