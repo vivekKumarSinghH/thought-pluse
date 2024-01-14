1c09fc522672f1d3af7fa98b55ff207b458aea25
Vivek Kumar <72158561+vivekKumarSinghH@users.noreply.github.com>
2024-01-14 17:54:08 +0530

    feat: :heavy_plus_sign: add search option in sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon, PlusCircle } from "lucide-react";
+import { ChevronsLeft, MenuIcon, PlusCircle, Search } from "lucide-react";
 import { usePathname } from "next/navigation";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -113,6 +113,7 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item onClick={() => null} label="Search" isSearch icon={Search} />
           <Item onClick={onCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
